67d878391a2aa6e5d3d687ad11c7ed848d37d2da
Aalococo <62804085+Aalococo@users.noreply.github.com>
2021-07-19 10:20:39 +0200

    Delete menderize-leaflet-image.sh

--- menderize-leaflet-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-MENDER_ARTIFACT_NAME=release-1 ./mender-convert \
-   --disk-image input/image_2021-05-31-consolinno-buster-lite.zip \
-   --config configs/leaflet_config \
-   --overlay rootfs_leaflet/
